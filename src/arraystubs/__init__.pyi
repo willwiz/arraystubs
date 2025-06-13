af8e3b1907e2b6eefea89cdf6c377f7302536520
Will Zhang <willwz@gmail.com>
2025-06-13 14:55:47 -0400

    update for numpy 2.3

--- src/arraystubs/__init__.pyi
+++ src/arraystubs/__init__.pyi
@@ -1,4 +1,6 @@
 # ruff: noqa: PYI042
+from __future__ import annotations  # noqa: PYI044
+
 __all__ = [
     "T2",
     "T3",
@@ -12,19 +14,31 @@ __all__ = [
     "flt",
     "int_t",
 ]
-from typing import Any, TypeAlias
+from typing import Any
 
 import numpy as np
 
-AShape: TypeAlias = tuple[int, ...]
-dbl: TypeAlias = np.dtype[np.float64]
-flt: TypeAlias = np.dtype[np.floating]
-int_t: TypeAlias = np.dtype[np.integer]
-char: TypeAlias = np.dtype[np.str_]
-bool_t: TypeAlias = np.dtype[np.bool_]
-type Vec[T: (flt, int_t, dbl, char, bool_t)] = np.ndarray[AShape, T]
-type Mat[T: (flt, int_t, dbl, char, bool_t)] = np.ndarray[AShape, T]
-type MatV[T: (flt, int_t, dbl, char, bool_t)] = np.ndarray[AShape, T]
+type AShape = tuple[int, ...]
+type dbl = np.dtype[np.float64]
+type flt = np.dtype[np.floating]
+type int_t = np.dtype[np.integer]
+type char = np.dtype[np.str_]
+type bool_t = np.dtype[np.bool_]
+type A1[T: (np.floating, np.float32, np.float64, np.integer, np.str_, np.bool_)] = np.ndarray[
+    tuple[int],
+    np.dtype[T],
+]
+type A2[T: (np.floating, np.float32, np.float64, np.integer, np.str_, np.bool_)] = np.ndarray[
+    tuple[int, int],
+    np.dtype[T],
+]
+type A3[T: (np.floating, np.float32, np.float64, np.integer, np.str_, np.bool_)] = np.ndarray[
+    tuple[int, int, int],
+    np.dtype[T],
+]
+type Vec[T: (flt, int_t, dbl, char, bool_t)] = np.ndarray[tuple[int], T]
+type Mat[T: (flt, int_t, dbl, char, bool_t)] = np.ndarray[tuple[int, int], T]
+type MatV[T: (flt, int_t, dbl, char, bool_t)] = np.ndarray[tuple[int, int, int], T]
 type Arr[T: np.dtype[Any]] = np.ndarray[tuple[int, ...], T]
 type T2[T: (float, int)] = tuple[T, T]
 type T3[T: (float, int)] = tuple[T, T, T]
